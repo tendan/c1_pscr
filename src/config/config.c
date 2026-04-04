c2f0cfd7016ebcd3618c09cc016902cfec5bf4ef
Maciej Ślusarz <maciej.slusarz@proton.me>
2026-04-04 21:21:17 +0200

    feat(config): Add configuration feature

---
+++ src/config/config.c
@@ -0,0 +1,5 @@
+//
+// Created by tendan on 4.04.2026.
+//
+
+#include "config.h"
---
+++ src/config/config.h
@@ -0,0 +1,8 @@
+//
+// Created by tendan on 4.04.2026.
+//
+
+#ifndef C1_CONFIG_H
+#define C1_CONFIG_H
+
+#endif //C1_CONFIG_H
