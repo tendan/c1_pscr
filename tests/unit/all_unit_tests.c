697fce1fbdba5acf5463b4944d71bf6a460f58d6
Maciej Ślusarz <maciej.slusarz@proton.me>
2026-04-04 20:27:23 +0200

    test(weather): Add Weather transformer tests

--- tests/unit/all_unit_tests.c
+++ tests/unit/all_unit_tests.c
@@ -4,7 +4,9 @@ static void run_all_tests(void)
 {
     RUN_TEST_GROUP(Grid);
     RUN_TEST_GROUP(WeatherClient);
+    RUN_TEST_GROUP(WeatherClientInternal);
     RUN_TEST_GROUP(WeatherParser);
+    RUN_TEST_GROUP(WeatherTransformer);
 }
 
 int main(int argc, const char *argv[])
