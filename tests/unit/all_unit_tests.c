3f01f9e02e906e5ffcbbaace8da45f729b0874fa
Maciej Ślusarz <maciej.slusarz@proton.me>
2026-04-05 19:08:07 +0200

    test(pipeline): Add pipeline related unit tests

--- tests/unit/all_unit_tests.c
+++ tests/unit/all_unit_tests.c
@@ -11,6 +11,7 @@ static void run_all_tests(void)
     RUN_TEST_GROUP(Config);
     RUN_TEST_GROUP(MqttPublisher);
     RUN_TEST_GROUP(SharedBuffer);
+    RUN_TEST_GROUP(Pipeline);
 }
 
 int main(int argc, const char *argv[])
